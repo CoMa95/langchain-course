e0bc140d56a48a8b2c5f0529f0d522685177af0d
CoMa95 <cosmin.manolescu95@gmail.com>
2026-08-20 09:53:02 +0100

    Add pydantic output formatting.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 
+from typing import List
+from pydantic import BaseModel, Field
+
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -21,22 +24,32 @@ from langchain_openai import ChatOpenAI
 #     """
 #     print(f'Searching for "{query}"')
 #     return tavily.search(query=query)
-# a
+
+
 # Option B - official tool from the Tavily-LangChain integration
 from langchain_tavily import TavilySearch
 
+# format output with pydantic objects
+class Source(BaseModel): # object for source, to be part of a nested agent response
+    """Schema for a source used by the agent"""
+    url:str = Field(description='The URL of the Source')
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description='List of sources used to generate the answer')
+
+# create LLM model, with tools and agents
 llm = ChatOpenAI(api_key=os.getenv('OPEN_API_KEY'))
 #tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model = llm, tools = tools )
+agent = create_agent(model = llm, tools = tools, response_format =  AgentResponse)
 
 def main():
     result = agent.invoke(
         {"messages":HumanMessage(content="What is the weather in Tokyo?")}
         )
     print(result)
-    
-# dg
 
 if __name__ == "__main__":
     main()
